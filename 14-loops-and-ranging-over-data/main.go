007b5b5fd2e41ff7763247e7ccfff1de6151b49f
Raven Dickerson <raejdickerson@gmail.com>
2022-12-18 16:23:41 -0600

    With identifier.

--- 14-loops-and-ranging-over-data/main.go
+++ 14-loops-and-ranging-over-data/main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	count := 5
-	for i := 0; i < count; i++ {
-		fmt.Println(i)
+	animals := []string{"dog", "fish", "cat", "bear", "cow", "horse"}
+
+	for i, animal := range animals {
+		fmt.Println(i, animal)
 	}
 }
