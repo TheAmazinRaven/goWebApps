1e1be1ea8045671251f0b7c6740792ba3fb268fa
Raven Dickerson <raejdickerson@gmail.com>
2022-12-18 11:47:46 -0600

    Learned about pointers.

---
+++ 9-pointers/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "log"
+
+func main() {
+	var myString string
+	myString = "Pink"
+
+	log.Println("myString is set to", myString)
+	changeUsingPointer(&myString)
+	log.Println("After the function has been called, myString is set to", myString)
+
+}
+
+func changeUsingPointer(s *string) {
+	log.Println("s is set to", s)
+	newValue := "Purple"
+	*s = newValue
+}
