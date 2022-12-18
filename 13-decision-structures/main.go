f75c0b4610fbc3d97688669491eb331cf9616d13
Raven Dickerson <raejdickerson@gmail.com>
2022-12-18 16:17:55 -0600

    String switch case.

--- 13-decision-structures/main.go
+++ 13-decision-structures/main.go
@@ -3,16 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	myNum := 100
-	isTrue := false
+	myVar := "apple"
 
-	if myNum > 99 && !isTrue {
-		fmt.Println("myNum is greater than 99 & isTrue is set to true.")
-	} else if myNum < 100 && isTrue {
-		fmt.Println("1")
-	} else if myNum == 101 || isTrue {
-		fmt.Println("2")
-	} else if myNum > 1000 && isTrue == false {
-		fmt.Println("3")
+	switch myVar {
+	case "cat":
+		fmt.Println("Cat is set to cat.")
+	case "dog":
+		fmt.Println("Cat is set to dog.")
+	case "fish":
+		fmt.Println("Cat is set to fish.")
+
+	default:
+		fmt.Println("Cat is set to something else.")
 	}
+
 }
