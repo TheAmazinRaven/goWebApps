97a934602e181915a2c34645bc1af1e56519f319
Raven Dickerson <raejdickerson@gmail.com>
2022-12-18 16:37:03 -0600

    Ranging over things.

--- 14-loops-and-ranging-over-data/main.go
+++ 14-loops-and-ranging-over-data/main.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-	firstLine := "I brush against those freckles that I hated so"
+type User struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Age       int
+}
 
-	for i, l := range firstLine {
-		fmt.Println(i, l)
+func main() {
+
+	var users []User
+	users = append(users, User{"Rae", "D.", "email@awesome.com", 29})
+	users = append(users, User{"Sally", "M.", "email@awesome.com", 41})
+	users = append(users, User{"Bob", "S.", "email@awesome.com", 12})
+	users = append(users, User{"Tenoch", "H.", "email@awesome.com", 35})
+
+	for _, l := range users {
+		fmt.Println(l.FirstName, l.LastName, l.Email, l.Age)
 	}
 }
